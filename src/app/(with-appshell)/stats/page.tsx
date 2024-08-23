2d31fb1856116e4a5cfca1245bd76e31b631e30f
Daniil <support@danyatochka.ru>
2024-08-23 17:02:13 +0300

    feat: добавил выбор кол-ва дней для статистики

--- src/app/(with-appshell)/stats/page.tsx
+++ src/app/(with-appshell)/stats/page.tsx
@@ -4,46 +4,90 @@ import {getServerSession} from "next-auth";
 import {authOptions} from "@/lib/auth";
 import {redirect} from "next/navigation";
 import {PAGE_LINKS} from "@/constants/page-links";
-import {addDays, format} from "date-fns";
+import {addDays, format, startOfDay} from "date-fns";
 
-type UsersCountResponse = { total_count: number, date: string }
+type UsersCountResponse = {
+    total_count: number;
+    details: Array<{
+        groups: Array<{
+            group_id: number;
+            group_name?: string;
+            inactive_count?: number;
+            count: number;
+        }>;
+        date: Date;
+    }>;
+}
 
-export default async function StatsPage() {
+function getFormattedPhrase(days: number) {
+    switch (days) {
+        case 7:
+            return 'неделю'
+        case 30:
+            return 'месяц'
+        default:
+            return `${days} дн.`
+    }
+}
+
+export default async function StatsPage(props: any) {
     const session = await getServerSession(authOptions)
 
     if (!session) {
         return redirect(PAGE_LINKS.LOGIN)
     }
 
-    const total: UsersCountResponse = await fetch(`${process.env.BOT_API_HOST}/info/users/count`).then(i => i.json())
-    const days: UsersCountResponse[] = await Promise.all(
-            Array.from({length: 7}, (_, index) => addDays(new Date(), -index))
-                    .map(d => fetch(`${process.env.BOT_API_HOST}/info/users/count?date=${format(d, 'yyyy-MM-dd')}`).then(i => i.json()))
-    )
+    const days = (+props.searchParams.days > 30 ? 30 : +props.searchParams.days) || 7
 
-    return  <Container p={'md'}>
+    const dates = Array.from({length: days}, (_, index) => startOfDay(addDays(new Date(), -index))).reverse()
+
+    const total: {
+        data: UsersCountResponse
+    } = await fetch(`${process.env.BOT_API_HOST}/info/users/count`).then(i => i.json())
+    const stats: {
+        data: UsersCountResponse
+    } = await fetch(`${process.env.BOT_API_HOST}/info/users/count?date=${format(new Date(), 'yyyy-MM-dd')}&days=${days}`)
+            .then(i => i.json())
+
+    // stats.data.details.toSorted((a,b) => new Date(a.date).getTime() - new Date(b.date).getTime()).forEach(a => console.log(a.date, a.groups.reduce((acc, group) => acc + group.count, 0)))
+
+    /*console.log(dates.map(d => ({
+        date: format(d, 'yyyy-MM-dd'),
+        'Новых пользователей за день': stats.data.details.find(j =>
+                format(j.date, 'yyyy-MM-dd') === format(d, 'yyyy-MM-dd')
+        )?.groups.reduce((acc, cur) => acc + cur.count, 0) || 0
+    })))*/
+
+    return <Container p={'md'}>
         <Stack gap={'lg'}>
             <Group justify={'space-between'}>
                 <Group align={'baseline'} gap={'xs'}>
                     <Text fw={700} size={'lg'}>Статистика бота</Text>
                 </Group>
             </Group>
-            <Text>Кол-во зарегистрированных пользователей: {total.total_count}</Text>
-            <AreaChart
-                    h={300}
-                    data={days.toSorted((a, b) => (new Date(a.date).getTime() - new Date(b.date).getTime())).map(i => ({
-                        date: format(i.date, 'yyyy-MM-dd'),
-                        'Новых пользователей за день': i.total_count,
-                        // 'Всего пользователей': total.total_count
-                    }))}
-                    dataKey="date"
-                    connectNulls
-                    series={[
-                        {name: 'Новых пользователей за день', color: 'brand.6'},
-                        // {name: 'Всего пользователей', color: 'brand.3'},
-                    ]}
-                    curveType="monotone"
-            />
+            <Stack gap={'xs'}>
+                <Text>Кол-во зарегистрированных пользователей: {total.data.total_count}</Text>
+                <Text>Новых пользователей за {getFormattedPhrase(days)}: {stats.data.total_count}</Text>
+            </Stack>
+            <Group mt={'md'}>
+                <AreaChart
+                        h={300}
+                        data={
+                            dates.map(d => ({
+                                date: format(d, 'yyyy-MM-dd'),
+                                'Новых пользователей за день': stats.data.details.find(j =>
+                                        format(j.date, 'yyyy-MM-dd') === format(d, 'yyyy-MM-dd')
+                                )?.groups.reduce((acc, cur) => acc + cur.count, 0) || 0
+                            }))
+                        }
+                        dataKey="date"
+                        connectNulls
+                        series={[
+                            {name: 'Новых пользователей за день', color: 'brand.6'}
+                        ]}
+                        curveType="monotone"
+                />
+            </Group>
         </Stack>
     </Container>
 }

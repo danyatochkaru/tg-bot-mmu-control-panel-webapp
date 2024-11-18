f46fd1d197af9c8ff21b374f30c078d51eb582aa
Daniil <support@danyatochka.ru>
2024-11-18 19:35:10 +0300

    refactor(api): перенёс api для приёма хуков

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -137,22 +137,3 @@ export async function POST(req: Request) {
         return NextResponse.json({message: 'Что-то пошло не так...'}, {status: 500})
     }
 }
-
-export async function PATCH(req: Request) {
-    try {
-        for (const [key, value] of Array.from(req.headers.entries())) {
-            console.log(key, value)
-        }
-
-        const session = await checkSession()
-        if (!session.data) {
-            return NextResponse.json(session.error, {status: session.status})
-        }
-
-        return NextResponse.json({ok: 'ok'})
-
-    } catch (e) {
-        console.error(e)
-        return NextResponse.json({message: 'Что-то пошло не так...'}, {status: 500})
-    }
-}
--- src/app/api/webhooks/messages/route.ts
+++ src/app/api/webhooks/messages/route.ts
@@ -1,3 +1,20 @@
-export async function POST(req: Request) {
+import checkSession from "@/utils/checkSession";
+import {NextResponse} from "next/server";
 
+export async function POST(req: Request) {
+    try {
+        for (const [key, value] of Array.from(req.headers.entries())) {
+            console.log(key, value)
+        }
+
+        const session = await checkSession()
+        if (!session.data) {
+            return NextResponse.json(session.error, {status: session.status})
+        }
+
+        return NextResponse.json({ok: 'ok'})
+    } catch (e) {
+        console.error(e)
+        return NextResponse.json({message: 'Что-то пошло не так...'}, {status: 500})
+    }
 }
